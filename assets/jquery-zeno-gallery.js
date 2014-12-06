56554f1bc8a6062c040cf3cc991498092f7d26f5
Richard Smith <richard@smith-net.org.uk>
2014-12-06 15:09:28 +0000

    add animation and makes the infinate scroll effect work - implements #1

--- assets/jquery-zeno-gallery.js
+++ assets/jquery-zeno-gallery.js
@@ -29,21 +29,6 @@
     //create some basic jquery pointers
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //grab the user agent and current hash string
     var user_gent = navigator.userAgent.toLowerCase();
     var url_hash  = window.location.hash.replace(/#\//, '');
@@ -60,13 +45,10 @@
         //setup an alternate to this
         var self = this;
 
-        //store for the current image show
-        this._current = 0;
-
         //extend the default options with the user provided ones
         this._options = $.extend({}, defaults, options);
 
-        //keep a reference to the gallery dom
+        //keep a reference to the various parts of the gallery dom
         this._element = element;
 
         //start adding all the markup needed by the gallery
@@ -75,13 +57,15 @@
         this._element.append('<div class="zg-control-bar"><div class="zg-thumbnails"/><div class="zg-slideshow"/><div class="zg-position"/><div class="zg-message"/><div class="zg-fullscreen"/></div>');
         this._element.children().wrapAll('<div class="zg-wrapper"/>');
 
-        //get the images and pile them into an array for reasons
-        this._images = [];
-        this._element.find('.zg-frame > div').each(function() {
-            var $this = $(this); self._images.push({ 'src' : $this.data('src'), 'title' : $this.data('title'), 'description' : $this.data('description') });
-        });
+        //store the current image shown and the total images we have 
+        this._current_image = 1; this._total_images = this._element.find('.zg-frame > div').length;
 
-        //bind the left/right arrow keys to the gallery previous/next functions
+        //clone the first and last element in the image data to allow for an infinite next/previous effect
+        var $images = this._element.find('.zg-frame > div');
+        $images.filter(':first').before($images.slice(-1).clone());
+        $images.filter(':last').after($images.slice(0, 1).clone());
+
+        //bind the left/right arrow keys to the galleries previous/next image functions
         this._element.attr('tabindex', 0).on('keypress', function(event) {
             if (event.keyCode == keys.left)  self.showPrevImage();
             if (event.keyCode == keys.right) self.showNextImage();
@@ -120,19 +104,15 @@
             this._options.height = this._element.height();
         }
 
-        //if the image height and viewport are mismatched add a bit of top margin
+        //update the width of the image containers to fill the gallery
+        this._element.find('.zg-frame > div').css('width', this._options.width);
+        
+        //set the displayed image to the current image - this resets the margin if the images change size
+        this._element.find('.zg-frame').css('margin-left', ((this._options.width * this._current_image) * -1));
 
-        // if (this._options.height !== this._element.find('img').css('width')) {
-        //     this._element.find('.zg-frame')
-        // }
-
-
-        //update the width of the images to fill the gallery
-        this._element.find('img').css('width', this._options.width);
-
-        //set soem default css to make everything work
+        //set some default css to make everything work
         this._element.find('.zg-frame').css({
-            'width'      : (this._options.width * this._images.length) + 100,
+            'width'      : (this._options.width * (this._total_images + 2)) + 100,
             'height'     : (this._options.height),
             'margin-top' : ((this._options.height / 2) * -1),
         });
@@ -141,42 +121,63 @@
     //load the image asset
     ZenoGallery.prototype.loadImage = function () {};
 
-    //show the previous or next image in our play cycle
-    ZenoGallery.prototype.showPrevImage = function () { this._current = (((this._current - 1) < 0) ? (this._images.length - 1) : (this._current - 1)); this.updateGallery(); };
-    ZenoGallery.prototype.showNextImage = function () { this._current = (((this._current + 1) >= this._images.length) ? 0 : (this._current + 1)); this.updateGallery(); };
+    //show the previous image in the gallery
+    ZenoGallery.prototype.showPrevImage = function () { 
+        console.info('ZenoGallery::showPrevImage()'); 
+        if (this._element.find('.zg-frame:not(:animated)').length > 0) {
+            this._current_image = (this._current_image - 1); 
+            this.updateGallery();
+        }
+    };
+
+    //show the next image in the gallery
+    ZenoGallery.prototype.showNextImage = function () { 
+        console.info('ZenoGallery::showNextImage()'); 
+        if (this._element.find('.zg-frame:not(:animated)').length > 0) {
+            this._current_image = (this._current_image + 1); 
+            this.updateGallery(); 
+        }
+    };
 
     //toggle the slidshow the slide show mode
     ZenoGallery.prototype.toggleSlideshow = function () {
-        this.toggleCall((this._slideshow !== undefined), 'startSlideshow', 'stopSlideshow');
+        console.info('ZenoGallery::toggleSlideshow()');
+        this.toggleCall((this._slideshow === undefined), 'startSlideshow', 'stopSlideshow');
     };
 
     //start the slide show mode
     ZenoGallery.prototype.startSlideshow = function () {
+        console.info('ZenoGallery::startSlideshow()');
         this._slideshow = window.setInterval(this.showNextImage.bind(this), this._options.delay);
     };
 
     //stop the slide show mode
     ZenoGallery.prototype.stopSlideshow = function () {
+        console.info('ZenoGallery::stopSlideshow()');
         window.clearTimeout(this._slideshow); delete this._slideshow;
     };
 
     //toggle the fullscreen display the slide show mode
     ZenoGallery.prototype.toggleFullscreen = function () {
+        console.info('ZenoGallery::toggleFullscreen()');
         this.toggleCall((this._element.zeno_fullscreen() === true), 'stopFullscreen', 'startFullscreen');
     };
 
     //display the gallery in full screen mode
     ZenoGallery.prototype.startFullscreen = function () {
+        console.info('ZenoGallery::startFullscreen()');
         this._element.find('.zg-wrapper').zeno_fullscreen('start');
     };
 
     //revert the gallery back to normal
     ZenoGallery.prototype.stopFullscreen = function () {
+        console.info('ZenoGallery::stopFullscreen()');
         this._element.find('.zg-wrapper').zeno_fullscreen('stop');
     };
 
     //update all the various display bits
     ZenoGallery.prototype.updateGallery = function () {
+        console.info('ZenoGallery::updateGallery()');
         this.updateControlBarPosition();
         this.updateControlBarMessage();
         this.updateDisplayedImage();
@@ -184,21 +185,35 @@
 
     //update the gallery to display the current image
     ZenoGallery.prototype.updateDisplayedImage = function () {
-        this._element.find('.zg-frame').css('margin-left', ((this._options.width * this._current) * -1) );
+        console.info('ZenoGallery::updateDisplayedImage()');
+
+        console.log('current image: ' + this._current_image);
+        console.log('current margin: ' + this._element.find('.zg-frame').css('margin-left'));
+
+        //animate the scroll, if we end up on the cloned elements - reset the display to the "real" element
+        this._element.find('.zg-frame').animate({ 'margin-left' : ((this._options.width * this._current_image) * -1) }, this._options.speed, function() {
+            if (this._current_image <= 0 || this._current_image > this._total_images) {
+                this._current_image = ((this._current_image > this._total_images) ? 1 : this._total_images);
+                this._element.find('.zg-frame').css('margin-left', ((this._options.width * this._current_image) * -1));
+            }
+        }.bind(this));
     };
 
     //update the position value on the main control bar
     ZenoGallery.prototype.updateControlBarPosition = function () {
-        this._element.find('.zg-control-bar > .zg-position').html((this._current + 1) + ' / ' + this._images.length);
+        console.info('ZenoGallery::updateControlBarPosition()');
+        this._element.find('.zg-control-bar > .zg-position').html((this._current_image) + ' / ' + this._total_images);
     };
 
     //use the meta data for the current image to populate the message bar
     ZenoGallery.prototype.updateControlBarMessage = function () {
-        this._element.find('.zg-control-bar > .zg-message').html('<b>' + this._images[this._current].title + '</b> ' + this._images[this._current].description);
+        console.info('ZenoGallery::updateControlBarMessage()');
+        //this._element.find('.zg-control-bar > .zg-message').html('<b>' + this._images[this._current_image].title + '</b> ' + this._images[this._current_image].description);
     };
 
     //toggle function - calls the function depending on the boolean state of expression
     ZenoGallery.prototype.toggleCall = function(expression, onTrue, onFalse) {
+        console.info('ZenoGallery::toggleCall()');
         return this[((expression === true) ? onTrue : onFalse)]();
     };
 
